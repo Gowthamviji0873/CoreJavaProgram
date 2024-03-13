894902b6883596b0fb691a30b3e70d3a40ad37cb
suriya <suriya@ByteNinja>
2024-03-13 12:40:15 +0530

    my java programs

---
+++ CoreJavaPrograms/src/day_1javabasics/username.java
@@ -0,0 +1,11 @@
+package day_1javabasics;
+import java.util.*;
+public class username {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter your name:");
+		String n=s.nextLine();
+		System.out.println("Hello "+n+" Coder");
+	}
+
+}
